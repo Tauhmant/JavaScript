665d006026427b49b50e15430d2b89da1fb3328c
Tauhmant <tauhmant@gmail.com>
2023-10-28 10:58:10 +0200

    update

--- larin_zewnetrzny.js
+++
@@ -1,2 +0,0 @@
-document.write("ostatnia modyfikacja strony".fontcolor("blue").bold().strike().fontsize(5)+"<br>");
-document.write(document.lastModified.fontcolor("blue").bold().strike().fontsize(5));
